15c96bfa6e9cb2409f656d7a2aec27b3191bfa09
ammarmughal <meet.sagi.here@gmail.com>
2020-05-16 08:06:42 +0500

    requests done

--- src/components/Data/requests.js
+++ src/components/Data/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import parsers from './parsers';
 
-async function usStats() {
+  const usStats = async () => {
   const response = await axios.get('https://covidtracking.com/api/v1/us/current.json');
 
   return parsers.usStats(response.data)
